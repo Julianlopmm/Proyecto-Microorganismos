b799f7a830fb72cc53f8ca1516b80dab966d15b0
Julianlopm <124851329+Julianlopmm@users.noreply.github.com>
2023-03-04 18:20:08 -0600

    Delete Proyecto Microorganismos directory

--- Proyecto Microorganismos/Proyecto Microorganismos/src/OrganismoVision.java
+++
@@ -1,6 +0,0 @@
-public class OrganismoVision extends OrganismoBasico{
-    
-    public OrganismoVision (int energia, int vision,int velocidad, int edad){
-        super(energia, vision, velocidad, edad);
-    }
-}
