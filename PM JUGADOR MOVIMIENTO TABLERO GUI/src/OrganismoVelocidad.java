0f68af431a320fad2d2f1ff73fd3bf483645d713
Julianlopm <124851329+Julianlopmm@users.noreply.github.com>
2023-03-07 20:20:09 -0600

    Delete PM JUGADOR MOVIMIENTO TABLERO GUI directory

--- PM JUGADOR MOVIMIENTO TABLERO GUI/src/OrganismoVelocidad.java
+++
@@ -1,6 +0,0 @@
-public class OrganismoVelocidad extends OrganismoBasico{
-    
-    public OrganismoVelocidad(int energia, int vision,int velocidad, int edad){
-        super(energia, vision, velocidad, edad);
-    }
-}
